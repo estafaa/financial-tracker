11b475e1d0904cffe7c4b759a3cfd3ae97642a08
Anton <faa@phystech.edu>
2017-11-02 00:46:56 +0000

    added user and stock assisiation, bu stock stoped to work

--- app/assets/javascripts/stocks.js
+++ app/assets/javascripts/stocks.js
@@ -4,7 +4,7 @@ var init_stock_lookup;
 init_stock_lookup = function(){
      $('#stock-lookup-form').on('ajax:before', function(event,data,status){
         show_spinner();
-    });
+    })
     
      $('#stock-lookup-form').on('ajax:after', function(event,data,status){
         hide_spinner();
